90e53801ee4759042a2a0808835665ccfde6aff9
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-30 12:32:07 -0400

    created the DragonBorn class.

---
+++ src/com/jeffwhenderson/topDown/DragonBorn.java
@@ -0,0 +1,27 @@
+package com.jeffwhenderson.topDown;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class DragonBorn extends GameObject {
+
+	public DragonBorn(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return null;
+	}
+
+}
